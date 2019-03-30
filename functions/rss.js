1a4a4bbfdd405e956f41f825530ea8b549847700
Sakamoto-san <skmt3p@gmail.com>
2019-03-30 17:52:24 +0900

    rss fixed

--- functions/rss.js
+++ functions/rss.js
@@ -5,6 +5,7 @@ exports.fetchRss = (db, funcReq, funcRes) => {
   const dayjs = require('dayjs')
   const batch = db.batch()
   const items = []
+  const LimitsReadRss = 20
   const oideyoList = [
     {
       themeId: 'oijavascript',
@@ -29,8 +30,8 @@ exports.fetchRss = (db, funcReq, funcRes) => {
     return new Promise((resolve, reject) => {
       try {
         console.log('setBatch: ', oideyo.themeId, oideyo.themeName)
-        for (let rss in oideyo.rssList) {
-          console.log('rss.url', rss.url)
+        for (let rss of oideyo.rssList) {
+          console.log('rss', rss)
           const req = request(rss.url)
           const feedparser = new FeedParser()
 
@@ -39,13 +40,11 @@ exports.fetchRss = (db, funcReq, funcRes) => {
             .on('error', error => {
               console.log('request error', error)
             })
-            .on('response', function(res) {
-              console.log('res: ', res)
-              const stream = this
+            .on('response', res => {
               if (res.statusCode !== 200) {
-                req.emit('error', new Error('Bad status code'))
+                return req.emit('error', new Error('Bad status code'))
               } else {
-                stream.pipe(feedparser)
+                return req.pipe(feedparser)
               }
             })
 
@@ -54,77 +53,82 @@ exports.fetchRss = (db, funcReq, funcRes) => {
             .on('error', error => {
               console.log('handle error', error)
             })
-            .on('meta', meta => {
-              console.log('==== %s ====', meta.title)
-            })
             .on('readable', () => {
-              let item = this.read()
-              if (item !== null) {
+              let stream = feedparser
+              let item = stream.read()
+              let counter = 0
+              while (item) {
+                counter++
                 items.push(item)
-                return true
-              } else {
-                console.log('items: count == 0')
-                return false
+                if (counter === LimitsReadRss - 1) {
+                  break
+                }
               }
             })
-            .on('end', parseEndFunc)
-          return resolve
+            .on('end', () => {
+              parseEndFunc(rss)
+            })
         }
+        return resolve
       } catch (e) {
         console.log(e)
         return new Error(reject)
       }
-      return resolve
     })
   }
 
-  const parseEndFunc = () => {
-    // 既存postsから同RSSの最新の1件を取得
-    let lastData = db
-      .collection('posts')
-      .where('url', '==', rss.url)
-      .orderBy('date', 'desc')
-      .limit(1)
+  const parseEndFunc = rss => {
+    try {
+      // 既存postsから同RSSの最新の1件を取得
+      const lastData = db
+        .collection('posts')
+        .where('url', '==', rss.url)
+        .orderBy('date', 'desc')
+        .limit(1)
 
-    // 今回取得したRSSからN件までが最新であるかを調べる
-    let limit = items.length - 1
-    if (lastData.date) {
-      for (limit; limit <= items.length; limit++) {
-        if (items[limit].date <= lastData.date) {
-          //limit = 0である場合はset対象が存在しないため後続処理は不要
-          if (limit === 0) {
-            limit = -1
+      // 今回取得したRSSからN件までが最新であるかを調べる
+      let limit = items.length - 1
+      if (lastData.date) {
+        for (limit; limit <= items.length; limit++) {
+          if (items[limit].date <= lastData.date) {
+            //limit = 0である場合はset対象が存在しないため後続処理は不要
+            if (limit === 0) {
+              limit = -1
+            }
+            break
           }
-          break
         }
       }
-    }
-    if (limit >= 0) {
-      // firestoreへのbatchでのset上限が500件であるため各RSSのlimitは最新20件迄とする
-      if (limit >= 20) {
-        limit = 19
-      }
-      console.log('items: set count == ', limit + 1)
-      for (let i = limit; i === 0; i--) {
-        item = items[i]
-        let uniqId = uniq()
-        const title = item.title
-        const url = item.link
-        const date = item.date
-        const imgUrl = item.image.url
-        const postData = {
-          id: `${3000000000 + dayjs().unix()}-${uniqId}`,
-          title: title,
-          url: url,
-          date: date,
-          tag: rss.tag,
-          img_url: imgUrl,
-          themeId: oideyo.themeId,
-          themeName: oideyo.themeName,
-          createdAt: 3000000000 + dayjs().unix()
+      if (limit >= 0) {
+        // firestoreへのbatchでのset上限が500件であるため各RSSのlimitは最新LimitsReadRss件迄とする
+        if (limit >= LimitsReadRss) {
+          limit = LimitsReadRss - 1
         }
-        batch.set(db.collection('posts'), postData)
+        console.log('items: set count == ', limit + 1)
+        for (let i = limit; i === 0; i--) {
+          item = items[i]
+          let uniqId = uniq()
+          const title = item.title
+          const url = item.link
+          const date = item.date
+          const imgUrl = item.image.url
+          const postData = {
+            id: `${3000000000 + dayjs().unix()}-${uniqId}`,
+            title: title,
+            url: url,
+            date: date,
+            tag: rss.tag,
+            img_url: imgUrl,
+            themeId: oideyo.themeId,
+            themeName: oideyo.themeName,
+            createdAt: 3000000000 + dayjs().unix()
+          }
+          batch.set(db.collection('posts'), postData)
+          console.log('finished: ', i, '件')
+        }
       }
+    } catch (e) {
+      console.log(e)
     }
   }
 
